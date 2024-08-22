8a1e9f6bc96739ca3a88a460f16f1b6f2bf5b832
usyar <kritin_v@protonmail.com>
2024-08-22 20:09:07 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,15 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+values_list = [2, 'str', False]
+values_dict = {'a': 1, 'b': 'str', 'c': 4.7}
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
